21dcb29f6b8003d030d8ba67bdd8d0825115dae7
Alexander Kubiak <akubiak@cps.edu>
2023-12-14 11:30:43 -0600

    done

--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -10,18 +10,29 @@ x = data[["Age", "EstimatedSalary", "Gender"]].values
 y = data["Purchased"].values
 
 # Step 1: Print the values for x and y
-
+print(x)
+print(y)
 # Step 2: Standardize the data using StandardScaler, 
-
+scaler = StandardScaler().fit(x)
 # Step 3: Transform the data
-
+x = scaler.transform(x)
 # Step 4: Split the data into training and testing data
-
+x_train, x_test, y_train, y_test = train_test_split(x, y)
 # Step 5: Fit the data
-
+scaler.fit(data)
 # Step 6: Create a LogsiticRegression object and fit the data
-
+logistic=linear_model.LogisticRegression()
+logistic.fit(x_train,y_train)
 # Step 7: Print the score to see the accuracy of the model
-
+print(logistic.score(x_test,y_test))
 # Step 8: Print out the actual ytest values and predicted y values
 # based on the xtest data
+print("real Y values:")
+print(y_test)
+print("predicted Y values:")
+print(logistic.predict(x_test))
+print(len(y_test))
+female_thirtyfour = [["34", "56000", "1"]]
+female_thirtyfour=scaler.transform(female_thirtyfour)
+my_prediction=logistic.predict(female_thirtyfour)
+print(my_prediction) 
--- part4-classification/a6_part4_writeup.md
+++ part4-classification/a6_part4_writeup.md
@@ -5,10 +5,14 @@ After completing `a6_part4.py` answer the following questions
 ## Questions to answer
 
 1. Comment out the StandardScaler and re-run your test. How accurate is the model? Why is that?
+Goes from 0.82 to 0.65. The value isn't too low but the model stil isn't very accurate because the value isn't very high either.
 
 2. How accurate is the model with the StandardScaler? Is this model accurate enough for the given use case? Explain.
+With StandardScaler, I think the model is accurate enough because the 0.82 indicates that it is good.
 
 3. Looking at the predicted and actual results, how did the model do? Was there a pattern to the inputs that the model was incorrect about?
+The model had some mistakes but I didn't notice any pattern in them.
 
 4. Would a 34 year old Female who makes 56000 a year buy an SUV according to the model? Remember to scale the data before running it through the model.
 
+No, a 34 year old Female who makes 56000 a year would not buy an SUV.
